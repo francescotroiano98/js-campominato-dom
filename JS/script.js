07567eaf8f914b83520120043c56aa435ffd2097
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-24 13:10:00 +0200

    add funcion random number

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,9 @@ buttonPlay.addEventListener("click", function(){
     boxElement.innerHTML = "";
     console.clear();
 
+    getRandomInt(minumNumber, maximumNumber)
+    getRandomUniqueNumber( minNum, maxNum, elements )
+
     for (let i = 1; i <= 100; i++) {
         createBox(i);
         
@@ -38,3 +41,59 @@ function createBox(number){
 
 }
 
+/**
+ * Function that generates an array of random unique numbers between two values (both included).
+ *
+ * @param minNum= The minimum interval for the random generated numbers
+ * @param maxNum The maximum interval for the random generated numbers
+ * @param elements The number of elements to be generated
+ * @returns The list of random unique generated numbers, or an empty array if it is not possibile to generate that amount of numbers within the given interval.
+ */
+
+function getRandomUniqueNumber( minNum, maxNum, elements ){
+  
+    const numbersList = [];
+  
+    if ( (maxNum - minNum) < elements ){
+  
+        return [];
+    }
+  
+    while (numbersList.length < elements){
+  
+        const newRandomNumber = getRandomInt(minNum, maxNum);
+  
+        if (!numbersList.includes(newRandomNumber)){
+  
+            numbersList.push(newRandomNumber);
+        }
+    }
+  
+    return numbersList;
+  }
+  
+  /**
+   * Function that generates a random number (not secure) between two values, both included.
+   *
+   * @param minumNumber the included minium value of the random generated number range.
+   * @param maximumNumber the included maximum value of the random generated number range
+   * @returns A randomly generated number.
+   */
+  
+  function getRandomInt(minumNumber, maximumNumber){
+  
+    const randomNumber = Math.floor( Math.random() * ( maximumNumber - minumNumber +1) + minumNumber);
+  
+    return randomNumber;
+  }
+  
+  //Il computer deve generare 16 numeri casuali 
+  const minNum = 1;
+  const maxNum = 100;
+  const elements = 16;
+  
+  
+  const numbersArray = getRandomUniqueNumber(minNum, maxNum, elements);
+  
+  console.log(numbersArray);
+  
